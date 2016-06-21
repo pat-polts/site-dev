3be62f1c9f21b98d1825455983152a0261c21c4b
poltts <poltss@gmail.com>
2016-06-21 14:58:23 -0300

    removed trash

---
+++ home.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+		<title>Patrícia Poltts .: webdeveloper | UX Designer :.</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	    <meta name="description" content="Demo project">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
+		<script src="js/player.js"></script> 
+		<script src="https://use.fontawesome.com/77d68e7a5b.js"></script>
+
+	</head>
+	<body> 
+		<main>
+			<video autoplay  preload="auto"  loop id="bg"> 
+				<source src="videos/website-video2.webm" type="video/webm">
+				<source src="videos/website-video.mp4" type="video/mp4">
+			</video>
+			<figure>			
+				<img src="public/imgs/me.jpg" alt="Profile image" title="Patricia Poltts profile image." />
+			</figure>	
+			<hgroup>	
+				<h1>Patrícia Poltts</h1> 
+				<h2>Webdeveloper | UX Designer </h2> 
+			</hgroup>
+		</main>
+	</body> 
+</html>
