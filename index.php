b0b50d5a17236c602efa6436e4ef600f79bc82c7
poltts <poltss@gmail.com>
2016-06-22 13:38:12 -0300

    nav menu markup

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
 <html lang="pt-BR">
 	<head>
+
 		<title>Patrícia Poltts, webdeveloper e ux designer portfolio site.</title>
+
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	    <meta name="description" content="Patrícia Poltts, profissional freelancer, há mais de 10 anos solucionando desafios e resolvendo bugs no mercado web.">
 	    <meta name="keywords" content="Webdeveloper, UX designer, Webdesigner, freelancer, web, especialista front-end, php, MySql, HTML 5, CSS 3, javascript, nodeJS, React JS, Laravel, CodeIgniter, websites, hot sites, programadora.">
     	<meta name="author" content="Patricia Poltts - patriciapoltts@gmail.com">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+
+	    <!-- Bootstrap Core CSS -->
+	    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+	    <!-- Custom CSS -->
+	    <link href="css/scrolling-nav.css" rel="stylesheet">
+
+	    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	    <!--[if lt IE 9]>
+	        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+	    
 		<link rel="stylesheet" href="css/style.css">
 		<script src="https://use.fontawesome.com/77d68e7a5b.js"></script>
 
@@ -29,16 +44,16 @@
 						<a href="#home"> home </a> 
 					</li>
 					<li>
-						<a href=""> habilidades </a>
+						<a href="#habilidades"> habilidades </a>
 					</li>
 					<li>
-						<a href=""> portfolio </a>
+						<a href="#portfolio"> portfolio </a>
 					</li>
 					<li>
-						<a href=""> blog </a>
+						<a href="#blog"> blog </a>
 					</li>
 					<li>
-						<a href=""> contato </a>
+						<a href="#contato"> contato </a>
 					</li>
 				</ul>
 			</nav>
@@ -64,8 +79,12 @@
 	    <!-- jQuery -->
 	    <script src="js/jquery.js"></script>
 
+	    <!-- Bootstrap Core JavaScript -->
+	    <script src="js/bootstrap.min.js"></script>
+
 	    <!-- Scrolling Nav JavaScript -->
 	    <script src="js/jquery.easing.min.js"></script>
 	    <script src="js/scrolling-nav.js"></script>
+
 	</body> 
 </html>
