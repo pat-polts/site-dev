05352d253aaa3cb2b7f3ce7f8c8b92e656dc4627
poltts <poltss@gmail.com>
2016-06-28 01:05:18 -0300

    set maintenance mode

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php include('header.php'); ?>
+
 	<div id="home">
 		<video autoplay  preload="auto"  loop id="media" tabindex="0" > 
 			<source src="videos/website-video.webm" type="video/webm">
 			<source src="videos/website-video.mp4" type="video/mp4">
 		</video>
 	</div>
- 
+
+ 	<span title="maintenance mode" class="maintenance-mode">maintenance mode</span>
+
 <?php include('footer.php'); ?>	
